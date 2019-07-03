0547d0778407d49f7fc772e79392298bea4c9577
Nizar <niezar.rk@gmail.com>
2019-07-03 08:23:37 +0700

    change api to https

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://api.pelaporan-dishub.xyz',
+    baseURL: 'https://api.pelaporan-dishub.xyz',
     headers: {
       'authorization': localStorage.getItem('token')
     }
